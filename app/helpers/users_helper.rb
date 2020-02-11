f4dc328163f647848a2c2d6117a06674cb6685fa
Reynard van den Berg <reyvdb@gmail.com>
2020-02-11 12:12:30 +0700

    add gravatar functionality

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,4 +7,11 @@ module UsersHelper
       "#{count} #{string.pluralize(count)}"
     end
 
+    def gravatar_for(user, size: 80)
+      #an MD5 hash algorithm (implemented using the 'hexdigest' method [part of the Digest library]) converts the email into an MD5 hash (case-sensitive)
+      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+      image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
+
 end
