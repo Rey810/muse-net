7678f2079287c8202c82e15d9c6aa86e8eecb7ce
Reynard van den Berg <reyvdb@gmail.com>
2020-02-12 11:16:17 +0700

    add devise_scope for signout

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   root :to => 'static_pages#home'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }  
-
+  
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   as :user do
     get  'signin', to: 'devise/sessions#new'
     post 'signin', to: 'devise/sessions#create'
