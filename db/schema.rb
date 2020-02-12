e8cf24e9e88daa3cf8a0c157fc80a83ca04c7a24
Reynard van den Berg <reyvdb@gmail.com>
2020-02-12 21:39:25 +0700

    image upload in development with minimagick

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_051554) do
+ActiveRecord::Schema.define(version: 2020_02_12_140327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2020_02_11_051554) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
