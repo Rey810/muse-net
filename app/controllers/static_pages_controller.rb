82465f2c6872334a5000489a6c0bb7aa9c352043
Reynard van den Berg <reyvdb@gmail.com>
2020-02-14 10:22:54 +0700

    add landing page render to static_pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,8 @@ class StaticPagesController < ApplicationController
             @feed = current_user.feed
             @comments = Comment.all
             @friendships = current_user.friends
+        else
+            render 'static_pages/landing_page'
         end
     end
 
