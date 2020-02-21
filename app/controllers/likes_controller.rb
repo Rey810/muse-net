2a59016169c66aed352465415ecdcca3a41903d0
Reynard van den Berg <reyvdb@gmail.com>
2020-02-21 10:28:28 +0700

    move flash message in Likes Controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,15 @@
 class LikesController < ApplicationController
     def create
         @like = Post.find(params[:post_id]).likes.create(user: current_user)
-        
+
         if @like.save
             @post = Post.find(params[:post_id])
             respond_to do |format|
-                format.html { flash[:success] = "You liked a post."
-                                redirect_back(fallback_location: root_path) }
+                format.html {   redirect_back(fallback_location: root_path) }
                 format.js
             end
+
+            flash[:success] = "You liked a post."
         else
             flash[:notice] = "You could't like that post."
             redirect_back(fallback_location: user_path)
@@ -21,10 +22,10 @@ class LikesController < ApplicationController
         if @like.destroy
             @post = Post.find(params[:post_id])
             respond_to do |format|
-                format.html { flash[:success] = "You unliked a post."
-                                redirect_back(fallback_location: user_path(user_path)) }
+                format.html { redirect_back(fallback_location: user_path(user_path)) }
                 format.js
             end
+            flash[:success] = "You unliked a post."
         else
             flash[:notice] = "You could't unlike that post."
             redirect_back(fallback_location: user_path(user_path))
